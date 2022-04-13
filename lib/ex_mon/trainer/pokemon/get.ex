d10750b63ba164aff3d891528e451073763f0d71
Wilton Souza <willsouzafilho@gmail.com>
2022-04-12 22:22:55 -0300

    Creating the trainer pokemon get method

---
+++ lib/ex_mon/trainer/pokemon/get.ex
@@ -0,0 +1,18 @@
+defmodule ExMon.Trainer.Pokemon.Get do
+  alias Ecto.UUID
+  alias ExMon.{Error, Repo, Trainer.Pokemon}
+
+  def call(id) do
+    case UUID.cast(id) do
+      :error -> {:error, Error.build_invalid_id_format()}
+      {:ok, uuid} -> get(uuid)
+    end
+  end
+
+  defp get(uuid) do
+    case Repo.get(Pokemon, uuid) do
+      nil -> {:error, Error.build_pokemon_not_found_error()}
+      pokemon -> {:ok, pokemon}
+    end
+  end
+end
